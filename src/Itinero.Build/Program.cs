4e8f41aefa6a3871e376d172d1e75e941d24f2ea
airbreather <airbreather@linux.com>
2017-04-25 08:31:24 -0400

    Add pretend node-based contraction code for fun. It's a lie, and the result will not be correct, but it serves as a point of comparison. Also includes my path change, the LOH compaction junk is gone, and does console logging.

--- src/Itinero.Build/Program.cs
+++ src/Itinero.Build/Program.cs
@@ -1,14 +1,26 @@
-﻿using System;
+﻿////#define LIE
+
+using System;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime;
-using System.Threading;
 
+#if LIE
+using Itinero.Algorithms.Contracted;
+using Itinero.Algorithms.Contracted.Witness;
+#else
 using Itinero.Algorithms.Contracted.EdgeBased;
 using Itinero.Algorithms.Contracted.EdgeBased.Witness;
+#endif
+
 using Itinero.Algorithms.Weights;
 using Itinero.Data.Contracted;
+using Itinero.Data.Contracted.Edges;
 using Itinero.Graphs.Directed;
 using Itinero.Osm.Vehicles;
+
+using static System.FormattableString;
+using static Itinero.Logging.Logger;
 
 namespace Itinero.Build
 {
@@ -23,28 +35,26 @@ namespace Itinero.Build
                 throw new Exception("Must be server.");
             }
 
+            Stopwatch sw = Stopwatch.StartNew();
             Constants.MemoryArrayFactory = new UnmanagedMemoryArrayFactory();
-
-            // compact the LOH on at most one Gen 2 GC run every 30 seconds.
-            // probably unnecessary at this point, actually...
-            new Thread(() =>
+            LogAction = (origin, level, message, parameters) =>
             {
-                while (true)
+                Console.WriteLine(Invariant($"{sw.ElapsedTicks / (double)Stopwatch.Frequency:N3} [{origin}]: {message}"));
+                if (parameters != null)
                 {
-                    GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
-                    Thread.Sleep(30000);
+                    foreach (var kvp in parameters)
+                    {
+                        Console.WriteLine(Invariant($"        [{kvp.Key}]: {kvp.Value}"));
+                    }
                 }
-            })
-            {
-                IsBackground = true
-            }.Start();
+            };
 
             File.Delete(LogFile);
 
             var profile = Vehicle.Car.Fastest();
 
             Log(1);
-            using (var fl1 = new FileStream(@"E:\planet-170306.routerdb", FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.RandomAccess))
+            using (var fl1 = new FileStream(@"C:\Temp\planet-170306.routerdb", FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.SequentialScan))
             {
                 RouterDb db = RouterDb.Deserialize(fl1);
                 Log(2);
@@ -59,6 +69,22 @@ namespace Itinero.Build
                     throw new NotSupportedException("derp");
                 }
 
+#if LIE
+                // vertex-based is ok when no complex restrictions found.
+                var contracted = new DirectedMetaGraph(ContractedEdgeDataSerializer.Size, weightHandler.MetaSize);
+                var directedGraphBuilder = new DirectedGraphBuilder<float>(db.Network.GeometricGraph.Graph, contracted, weightHandler);
+                directedGraphBuilder.Run();
+
+                Log(3);
+
+                // contract the graph.
+                var priorityCalculator = new EdgeDifferencePriorityCalculator(contracted, new DykstraWitnessCalculator(int.MaxValue));
+                priorityCalculator.DifferenceFactor = 5;
+                priorityCalculator.DepthFactor = 5;
+                priorityCalculator.ContractedFactor = 8;
+                var hierarchyBuilder = new HierarchyBuilder<float>(contracted, priorityCalculator, new DykstraWitnessCalculator(int.MaxValue), weightHandler);
+                hierarchyBuilder.Run();
+#else
                 var contracted = new DirectedDynamicGraph(weightHandler.DynamicSize);
                 var directedGraphBuilder = new DirectedGraphBuilder<float>(db.Network.GeometricGraph.Graph, contracted, weightHandler);
                 directedGraphBuilder.Run();
@@ -72,6 +98,7 @@ namespace Itinero.Build
                 priorityCalculator.ContractedFactor = 8;
                 var hierarchyBuilder = new HierarchyBuilder<float>(contracted, priorityCalculator, new DykstraWitnessCalculator<float>(weightHandler, int.MaxValue, 64), weightHandler, db.GetGetRestrictions(profile, null));
                 hierarchyBuilder.Run();
+#endif
 
                 Log(4);
 
